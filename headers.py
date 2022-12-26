5ac17b9372b988d774c5fa253e3496cb2b44ffc4
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2022-12-26 18:03:30 +0530

    Add files via upload

---
+++ headers.py
@@ -0,0 +1,19 @@
+import requests
+
+headers = {
+'Host': 'tataplay.live.ott.irdeto.com',
+'Connection': 'keep-alive',
+'sec-ch-ua-mobile': '?0',
+'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+'sec-ch-ua-platform': 'Windows',
+'Accept': '*/*',
+'Origin': 'https://watch.tataplay.com',
+'Sec-Fetch-Site': 'cross-site',
+'Sec-Fetch-Mode': 'cors',
+'Sec-Fetch-Dest': 'empty',
+'Referer': 'https://watch.tataplay.com/',
+'Accept-Language': 'en-US,en-IN;q=0.9,en;q=0.8',
+'Accept-Encoding': 'gzip, deflate',
+'Content-Length': '4236',
+    
+}
